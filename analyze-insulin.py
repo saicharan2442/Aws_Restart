198743d6f2cbdd3b46e7f6fbe757119fb8f9fb8c
saicharan2442 <ffmails500@gmail.com>
2025-10-03 03:53:06 +0000

    second commit

---
+++ analyze-insulin.py
@@ -0,0 +1,8 @@
+# Exercise 1: Retrieving the protein sequence of human preproinsulin
+'''The National Center for Biotechnology Information (NCBI) has information on many biological sequences.
+
+Access NCBI at https://ncbi.nlm.nih.gov.
+
+Next to the search bar, choose the dropdown menu and select Protein. Next, in the search bar, enter human insulin and choose Search.'''
+
+#Exercise 2: Obtaining the protein sequence of human insulin
